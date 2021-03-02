aab4945d2e1cb65dc68954e0bca6de8f89840eec
miyajii-ds <75560193+miyajii-ds@users.noreply.github.com>
2021-03-02 09:10:47 +0900

    Update ifttt_twitter_alerm.py

--- ifttt_twitter_alerm.py
+++ ifttt_twitter_alerm.py
@@ -1,14 +1,5 @@
 from flask import Flask, request, abort
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
 
 # 環境変数取得のため。
 import os
@@ -25,15 +16,9 @@ app = Flask(__name__)
 # 大事な情報は環境変数から取得。
 IFTTT_KEY = os.environ['IFTTT_KEY']
 
-
-
 def ifttt_webhoook(event_id):
 	ifttt_url = 'https://maker.ifttt.com/trigger/'+event_id+'/with/key/'+IFTTT_KEY
-	
 	response = requests.post(ifttt_url)
-
-
-
 
 # 必須ではないけれど、サーバに上がったとき確認するためにトップページを追加しておきます。
 @app.route('/')
@@ -42,28 +27,10 @@ def top_page():
 
 
 # ユーザがメッセージを送信したとき、この URL へアクセスが行われます。
-@app.route('/callback', methods=['POST'])
-def callback_post():
-    # get X-Line-Signature header value
-    #signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    #body = request.get_data(as_text=True)
-    #app.logger.info('Request body: ' + body)
-
-    # handle webhook body
-    #try:
-    #    handler.handle(body, signature)
-    #except InvalidSignatureError:
-    #    abort(400)
-    
-    ifttt_webhoook(webhooks_test)
-
-    return 'OK'
-
-
-
-
+#@app.route('/callback', methods=['POST'])
+#def callback_post():
+#    ifttt_webhoook(webhooks_test)
+#    return 'OK'
 
 if __name__ == '__main__':
     app.run()
